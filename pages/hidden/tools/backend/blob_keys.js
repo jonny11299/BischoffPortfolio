25b971def61ce4ea1b338313da4d47124c35752a
Jonny Bischoff <jonnyb11299@gmail.com>
2026-03-16 17:04:48 -0700

    Can now post and get blobs

---
+++ pages/hidden/tools/backend/blob_keys.js
@@ -0,0 +1,44 @@
+
+
+// will require the webpage to also include tool_server.js
+
+function post_blob(cell, content, onSuccess = (response) => { }, onFail = (response) => { }) {
+    console.log(`Posting blob at ${location}`);
+    console.log(content);
+    post("blob", content, cell, onSuccess, onFail);
+}
+
+async function get_blob(cell) {
+    console.log("Getting blob at cell " + cell);
+    const obj = await get('get_blob', cell);
+    console.log("Got blob:");
+    console.log(obj);
+}
+
+
+
+// stores a list of keys for different blobs
+const blob_keys = [
+    {
+        name: 'copypaste_body',
+        row: 10,
+        column: 'a'
+    },
+    {
+        name: 'blank',
+        row: 0,
+        column: 'a'
+    }
+]
+
+
+
+function getLocation(name) {
+    for (let key of blob_keys) {
+        if (key.name === name) {
+            return `${key.column}${key.row}`;
+        }
+    }
+
+    return null;
+}
